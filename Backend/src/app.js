2b010fe6d3cd878eeb827447f9bdc5d87aa09a3b
piyush <piyushujgaokar544@gmail.com>
2026-05-14 22:32:49 +0530

    first commit

---
+++ Backend/src/app.js
@@ -0,0 +1,12 @@
+const cookieParser = require('cookie-parser');
+const express = require('express');
+
+
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser())
+
+
+
+module.exports = app;
